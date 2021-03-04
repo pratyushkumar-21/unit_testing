7f4495d8f771242bd32c340e08cd0656f3ca6575
pratyush kumar <prkusid@gmail.com>
2021-03-04 22:10:23 +0530

    now using pytest

--- test_valid_card.py
+++ test_valid_card.py
@@ -1,19 +1,13 @@
-import unittest
+import pytest
 from validate_card import valid_card, card_brand
 
-class TestCard(unittest.TestCase):
+def test_card():
+    assert valid_card("340000000000009") == True
+    assert valid_card("4111111111111112") == False
+    assert valid_card("4111111111111111") == True
+    assert valid_card("6522111111111119") == True
 
-    def test_card(self):
-        self.assertEqual(valid_card("340000000000009"), True)
-        self.assertEqual(valid_card("4111111111111112"), False)
-        self.assertEqual(valid_card("4111111111111111"), True)
-        self.assertEqual(valid_card("6522111111111119"), True)
-
-    def test_card_brand(self):
-        self.assertEqual(card_brand("340000000000009"), "american express")
-        self.assertEqual(card_brand("4111111111111111"), "visa")
-        self.assertEqual(card_brand("4111111111111111"), "visa")
-        self.assertEqual(card_brand("6522111111111119"), "rupay")
-
-if __name__ == '__main__':
-    unittest.main()
+def test_card_brand():
+    assert card_brand("340000000000009") == "american express"
+    assert card_brand("4111111111111111") == "visa"
+    assert card_brand("6522111111111119") == "rupay"
